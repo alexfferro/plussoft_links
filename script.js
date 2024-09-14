2c973739c21df74cd28e5efefb0ea920f1a5ae37
Alex Ferro <alex.ferreira.ferro@gmail.com>
2024-09-14 09:09:05 -0300

    updated

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function copy(id) {
-    const texto = document.getElementById(`${id}`).innerText;
+    const texto = document.getElementById(`${id}`).value;
     navigator.clipboard.writeText(texto)
-    checked(id)
-  }
-  
-  function checked(id) {
-    var check = document.getElementById(`${id}`)
-    check.className = 'check';
-  
-    setTimeout(function() {
-      check.classList.remove('check');
-    }, 2000)
   }
   
   function saveNote(){
@@ -18,12 +8,37 @@ function copy(id) {
     localStorage.setItem('note', content);
   }
   
-  window.onload = function(){
-    const savedNote = localStorage.getItem('note');
-    if (savedNote){
-      document.getElementById('notepad').value = savedNote;
-    }
-  };
+  // window.onload = function(){
+  //   const savedNote = localStorage.getItem('note');
+  //   if (savedNote){
+  //     document.getElementById('notepad').value = savedNote;
+  //   }
+  // };
   
-  document.getElementById('save-btn').addEventListener('click', saveNote);
-  
+  // document.getElementById('save-btn').addEventListener('click', saveNote)
+
+  function showModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";  // Exibe o modal
+
+    // Fecha o modal quando clicar no "x"
+    var closeModal = document.getElementsByClassName("close")[0];
+    closeModal.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // Fecha o modal se clicar fora da área do modal
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+}
+
+// Adiciona o evento de clique a todos os botões que possuem a classe 'open-modal-btn'
+var buttons = document.querySelectorAll(".open-modal-btn");
+buttons.forEach(function(button) {
+    button.onclick = function() {
+        showModal();  // Chama a função showModal
+    };
+});
