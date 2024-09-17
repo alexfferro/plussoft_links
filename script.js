4674e13f5a8b05b40704dc0b8bdb48d239f8d51a
Alex Ferro <alex.ferreira.ferro@gmail.com>
2024-09-17 17:07:25 -0300

    adicionado os links do SwitchApp

--- script.js
+++ script.js
@@ -7,33 +7,20 @@ function copyPhone(id) {
     const input = document.getElementById(`${id}`).value
     navigator.clipboard.writeText(input)
   }
-  
   function saveNote(){
     const content = document.getElementById('notepad').value;
     localStorage.setItem('note', content);
   }
-  
-  // window.onload = function(){
-  //   const savedNote = localStorage.getItem('note');
-  //   if (savedNote){
-  //     document.getElementById('notepad').value = savedNote;
-  //   }
-  // };
-  
-  // document.getElementById('save-btn').addEventListener('click', saveNote)
-
   function showModal() {
     var modal = document.getElementById("myModal");
-    modal.style.display = "block"; // Mostra o modal
-    // Fecha o modal automaticamente após 1 segundo
+    modal.style.display = "block";
     setTimeout(function() {
         modal.style.display = "none";
     }, 500); // 1000ms = 1 segundo
 }
-// Adiciona o evento de clique a todos os botões que possuem a classe 'open-modal-btn'
 var buttons = document.querySelectorAll(".open-modal-btn");
 buttons.forEach(function(button) {
     button.onclick = function() {
-        showModal();  // Chama a função showModal
+        showModal();
     };
 });
